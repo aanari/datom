99554d77465fff317cfdbd814cf56be146346b0b
Ali Anari <ali@anari.io>
2017-01-21 17:16:27 -0800

    Split into multiple pages, add contact, another testimonial

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 jQuery( document ).ready(function( $ ) {
-"use strict"
+  "use strict"
 
   $('.page-scroll').on('click', function (event) {
     var $anchor = $(this);
@@ -9,39 +9,10 @@ jQuery( document ).ready(function( $ ) {
     event.preventDefault();
   });
 
-
-/*-----------------------------------------------------------------------------------*/
-/*		STICKY NAVIGATION
-/*-----------------------------------------------------------------------------------*/
-$(".sticky").sticky({topSpacing:0});
 /*-----------------------------------------------------------------------------------*/
 /* 	LOADER
 /*-----------------------------------------------------------------------------------*/
-$("#loader").delay(500).fadeOut("slow");
-/*-----------------------------------------------------------------------------------
-    TESTNMONIALS STYLE 1
-/*-----------------------------------------------------------------------------------*/
-$('#carousel').flexslider({
-    animation: "slide",
-    controlNav: false,
-    animationLoop: false,
-    slideshow: false,
-    itemWidth: 122,
-    itemMargin: 5,
-    asNavFor: '#slider'
-});
-$('#slider').flexslider({
-    animation: "fade",
-    controlNav: false,
-    animationLoop: false,
-    slideshow: false,
-    sync: "#carousel"
-  });
-  
-  
-$('.testi-slides-flex').flexslider({
-  animation: "slide"
-});
+$("#loader").delay(200).fadeOut("fast");
 
 /*-----------------------------------------------------------------------------------*/
 /* 	SLIDER REVOLUTION
@@ -56,72 +27,23 @@ jQuery('.tp-banner-fix').show().revolution({
 	navigationStyle:"preview4",
 	parallax:"mouse",
 	parallaxBgFreeze:"on",
-	parallaxLevels:[7,4,3,2,5,4,3,2,1,0],												
-	keyboardNavigation:"on",						
+	parallaxLevels:[7,4,3,2,5,4,3,2,1,0],
+	keyboardNavigation:"on",
 	fullWidth:"off",
 	fullScreen:"off"
 });
 /*-----------------------------------------------------------------------------------*/
 /* 	SINGLE SLIDE
 /*-----------------------------------------------------------------------------------*/
-$(".single-slides").owlCarousel({ 
+  $(".single-slides").owlCarousel({
     items : 1,
-	autoplay:false,
-	autoplayHoverPause:true,
-	singleItem	: true,
-	navText: ["<i class='fa fa-angle-left'></i>","<i class='fa fa-angle-right'></i>"],
-	lazyLoad:true,
-	nav: true,
-	loop: true,
-	animateOut: 'fadeOut'	
+    autoplay:false,
+    autoplayHoverPause:true,
+    singleItem	: true,
+    navText: ["<i class='fa fa-angle-left'></i>","<i class='fa fa-angle-right'></i>"],
+    lazyLoad:true,
+    nav: true,
+    loop: true,
+    animateOut: 'fadeOut'	
+  });
 });
-});
-
-/*-----------------------------------------------------------------------------------*/
-/*    CONTACT FORM
-/*-----------------------------------------------------------------------------------*/
-function checkmail(input){
-  var pattern1=/^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
-  	if(pattern1.test(input)){ return true; }else{ return false; }}     
-    function proceed(){
-    	var name = document.getElementById("name");
-		var email = document.getElementById("email");
-		var company = document.getElementById("company");
-		var web = document.getElementById("website");
-		var msg = document.getElementById("message");
-		var errors = "";
-		if(name.value == ""){ 
-		name.className = 'error';
-	  	  return false;}    
-		  else if(email.value == ""){
-		  email.className = 'error';
-		  return false;}
-		    else if(checkmail(email.value)==false){
-		        alert('Please provide a valid email address.');
-		        return false;}
-		    else if(company.value == ""){
-		        company.className = 'error';
-		        return false;}
-		   else if(web.value == ""){
-		        web.className = 'error';
-		        return false;}
-		   else if(msg.value == ""){
-		        msg.className = 'error';
-		        return false;}
-		   else 
-      {
-
-  $.ajax({
-    url: "https://formspree.io/ali@datomsolutions.com",
-    method: "POST",
-    data: $("#contact_form").serialize(),
-    dataType: "json",
-		success: function(msg){
-		if(msg){
-			$('#contact_form').fadeOut(1000);
-			$('#contact_message').fadeIn(1000);
-				document.getElementById("contact_message");
-			 return true;
-		}}
-	});
-}};
